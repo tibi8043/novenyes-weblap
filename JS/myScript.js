c4ebe693a5243a3e53c18ca071274d185268e81f
Tibor Aux <tiboraux@gmail.com>
2021-05-24 11:08:47 +0200

    Kezdőlap szépítgetés
    Animációk hozzáadása logo elkészítése.

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,7 +1,7 @@
-window.addEventListener("load",init,false);
-function init(){
+window.addEventListener("load", init, false);
+function init() {
+}
+function $(id) {
+  return document.getElementById(id);
+}
 
-}
-function $(id){
-    return document.getElementById(id);
-}
