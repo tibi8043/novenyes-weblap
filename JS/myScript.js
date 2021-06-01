b6584ff75ea0452f0e3b96a1a2a656009b6d6306
Tibor Aux <tiboraux@gmail.com>
2021-06-01 19:00:07 +0200

    szépítgetések
    ...

--- JS/myScript.js
+++ JS/myScript.js
@@ -11,7 +11,7 @@ function regisztracio(){
 
 }
 function bejelentkezes(){
-  
+
 }
 
 
--- JS/viragok.js
+++ JS/viragok.js
@@ -5,12 +5,26 @@ function $(id) {
 }
 
 function init() {
-  $("zolborsolevelSzenecio")
+  
 }
-let zolborsolevelSzenecio={
+function fuggveny(){
+  let viz= $("viz").value;
+  let nap =$("nap").value;
+  let homerseklet =("homerseklet").value;
+}
+var viragok = new Array();
+viragok.push(zoldborsolevelSzenecio)
+let zoldborsolevelSzenecio={
     név: "Zöldborsólevelű szeneció",
     ár: 1500,
     vizigeny: 1,
     napigeny: 3,
     homerseklet: 3,
+}
+let tesztvirag={
+  név: "tesztvirag",
+  ár: 1500,
+  vizigeny: 2,
+  napigeny: 3,
+  homerseklet: 1,
 }
