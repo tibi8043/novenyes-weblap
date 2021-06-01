813e3bdf5cc4da2e34c2f7c8ba65c3e220460505
Tibor Aux <tiboraux@gmail.com>
2021-06-01 14:50:32 +0200

    Jelmagyarázat
    Jelmagyarázat, szűrések előkészítése, Kis ikonok elkészítése.

---
+++ JS/viragok.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", init, false);
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+function init() {
+  $("zolborsolevelSzenecio")
+}
+let zolborsolevelSzenecio={
+    név: "Zöldborsólevelű szeneció",
+    ár: 1500,
+    vizigeny: 1,
+    napigeny: 3,
+    homerseklet: 3,
+}
