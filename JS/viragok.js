ca487da430f426516fcb17752d889ce8918fd0fa
Tibor Aux <tiboraux@gmail.com>
2021-06-02 12:02:42 +0200

    folytatás
    ..

--- JS/viragok.js
+++ JS/viragok.js
@@ -5,7 +5,7 @@ function $(id) {
 }
 
 function init() {
-  
+  $("kereses").addEventListener("click", vizkivalogato,false);
 }
 function fuggveny(){
   let viz= $("viz").value;
@@ -13,18 +13,28 @@ function fuggveny(){
   let homerseklet =("homerseklet").value;
 }
 var viragok = new Array();
-viragok.push(zoldborsolevelSzenecio)
+
 let zoldborsolevelSzenecio={
     név: "Zöldborsólevelű szeneció",
     ár: 1500,
     vizigeny: 1,
     napigeny: 3,
-    homerseklet: 3,
+    homerseklet: 3
 }
 let tesztvirag={
   név: "tesztvirag",
   ár: 1500,
   vizigeny: 2,
   napigeny: 3,
-  homerseklet: 1,
+  homerseklet: 1
+}
+viragok.push(zoldborsolevelSzenecio, tesztvirag);
+function vizkivalogato(){
+  let vizesTömb = new Array();
+  for(i = 0; i<viragok.length; i++){
+    if(viragok[i].vizigeny = $("viz").value-1){
+      vizesTömb.push(viragok[i]);
+    }
+  }
+  console.log(vizesTömb);
 }
