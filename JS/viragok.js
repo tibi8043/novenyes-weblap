fa06e596c47c67cb711b6443a0f9e1b2de1e3053
Tibor Aux <tiboraux@gmail.com>
2021-06-03 23:02:04 +0200

    comments

--- JS/viragok.js
+++ JS/viragok.js
@@ -9,7 +9,9 @@ function init() {
 }
 
 function searchproduct() {
+  //Az input lesz a keresett szó
   const input = $("filter").value.toUpperCase();
+  //Kiválasztom a kártyák(növények) nevét
   const cardContainer = $("egész");
   const cards = cardContainer.getElementsByClassName("col-6");
 
@@ -17,7 +19,8 @@ function searchproduct() {
     let title = cards[i].querySelector(" .card .card-body h5.card-title");
     
     
-    
+    //Ha megegyeznek akkor láthatóvá teszem, ha nem akkor nem
+    //Fontos, hogy átkonvertálok mindent nagybetűvé hogy az majd a keresésnél ne okozzon problémát. 
     if (title.innerText.toUpperCase().indexOf(input) > -1) {
       cards[i].classList.replace("d-none","d-flex");
       console.log(cards[i].className);
